3ef38d1ca779d34ce05498dc2165f90910f31e7b
peco-tanaka <ok.computeruk1929@gmail.com>
2025-02-28 20:27:01 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,20 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:success] = "room successfully created"
+      redirect_to room_path
+    else
+      flash[:error] = "Something went wrong"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
